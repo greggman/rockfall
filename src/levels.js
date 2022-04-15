5acd9a898544e12e56fb833af726ce99feffa874
Gregg Tavares <github@greggman.com>
2022-04-15 01:02:24 -0700

    add 2 levels

--- src/levels.js
+++ src/levels.js
@@ -4,6 +4,8 @@ const levelPaths = [
   'tiled/hatchery.tmj',     // 2
   'tiled/random-start.tmj', // 3
   'tiled/happy-face.tmj',   // 4
+  'tiled/exit-test.tmj',    // 5
+  'tiled/grid.tmj',         // 6
 ];
 
 export default levelPaths;
