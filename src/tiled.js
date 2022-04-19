74257bb748c7ad9fb0c3475385c7f8005552eb77
Gregg Tavares <github@greggman.com>
2022-04-18 20:15:02 -0700

    Use directional bits from tiled, handle no tile

--- src/tiled.js
+++ src/tiled.js
@@ -1,10 +1,34 @@
 /* eslint-env browser */
 
 import {
+  kUp,
+  kDown,
+  kLeft,
+  kRight,
+} from './directions.js';
+import {
   kSymBorder,
   // kSymDiamond,
 } from './symbols.js';
 import { basenameNoExt } from './utils.js';
+
+const kHFlip = 0x80000000;
+const kVFlip = 0x40000000;
+const kSwap  = 0x20000000;
+const kDirBits = kHFlip | kVFlip | kSwap;
+const kIdBits  = 0xFFFFFFFF ^ kDirBits;
+
+
+const tiledDirBitsToDir = new Map([
+  [0,                           0 ],   // F
+  [kVFlip,                  kDown ],
+  [kHFlip,                  kLeft ],
+  [kVFlip | kHFlip,         kDown ],
+  [kSwap | 0,               kLeft ],
+  [kSwap | kVFlip,          kLeft ],
+  [kSwap | kHFlip,          kRight],
+  [kSwap | kVFlip | kHFlip, kRight],
+]);
 
 export function parseTiledLevel(data, url) {
   // add space for a border
@@ -18,9 +42,15 @@ export function parseTiledLevel(data, url) {
 
   // copy each line into the middle
   map.fill(kSymBorder);
-  const tiles = data.layers[0].data.map(v => v - 1);
+  const tiles = data.layers[0].data.map(v => Math.max(0, v - 1));
   for (let y = 0; y < data.height; ++y) {
     map.set(tiles.slice(y * data.width, (y + 1) * data.width), (y + 1) * mapWidth + 1);
+  }
+
+  for (let i = 0; i < map.length; ++i) {
+    const v = map[i];
+    map[i] = v === 0xFFFFFFF ? 0 : (v & kIdBits);
+    mapFlags[i] = tiledDirBitsToDir.get(v & kDirBits);
   }
 
   const settings = {name: basenameNoExt(url)};
