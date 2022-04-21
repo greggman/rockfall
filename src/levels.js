10c1614078a82467cfbae9495eb5a9a127218a8c
Gregg Tavares <github@greggman.com>
2022-04-21 00:15:00 -0700

    add level

--- src/levels.js
+++ src/levels.js
@@ -1,15 +1,15 @@
 const levelPaths = [
-                            // 0 - random
-  'tiled/magic-wall.tmj',   // 1
-  'tiled/hatchery.tmj',     // 2
-  'tiled/random-start.tmj', // 3
-  'tiled/happy-face.tmj',   // 4
-  'tiled/exit-test.tmj',    // 5
-  'tiled/amoeba.tmj',       // 6
-  'tiled/grid.tmj',         // 7
-  'tiled/down-n-out.tmj',   // 8
-  'tiled/vault.tmj',        // 9
-  'tiled/test-map.tmj',     // 10
+  'tiled/magic-wall.tmj',
+  'tiled/happy-face.tmj',
+  'tiled/random-start.tmj',
+  'tiled/exit-test.tmj',
+  'tiled/amoeba.tmj',
+  'tiled/hatchery.tmj',
+  'tiled/hive.tmj',
+  'tiled/down-n-out.tmj',
+  'tiled/grid.tmj',
+  'tiled/vault.tmj',
+  'tiled/test-map.tmj',
 ];
 
 export default levelPaths;
